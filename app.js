effb39ca54e41231d36d24889e187864d0f36db1
joel spencer <joelspencer@joels-MBP.lan>
2018-10-17 20:48:11 +0100

    Simple echo with app credentials

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ function () {
 });  
 // chat connector for communicating with the Bot Framework Service 
 var connector = new builder.ChatConnector({     
-    appId: process.env.MICROSOFT_APP_ID,     
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "2635d20b-b9ac-4670-aa4a-38db0daf3573",     
+    appPassword: "syggeWDC16424~|;ikTAFA{"
 });
 // Listen for messages from users  
 server.post('/api/messages', connector.listen());  
