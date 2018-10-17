06081934321b038f88b77c2fb66cfbfa79ed89b9
joel spencer <joel@jellycatdigital.co.uk>
2018-10-17 22:27:54 +0100

    Adds bot storage definition

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ server.listen(process.env.PORT || 3000, function()
 // Create chat bot
 var connector = new builder.ChatConnector
 ({ appId: 'd14a3195-d834-4185-8fad-21248b3d0c8a', appPassword: 'mrnrDN2+%dmsXGDPE2814~*' }); 
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector,
+{
+	storage: new builder.MemoryBotStorage()
+});
 server.post('/api/messages', connector.listen());
 // Create bot dialogs
 bot.dialog('/', function (session) {
