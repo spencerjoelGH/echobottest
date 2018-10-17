d5d54a274807cac1a1f1789c7a8a09244805c628
joel spencer <joelspencer@joels-MBP.lan>
2018-10-17 21:55:04 +0100

    Removes app credentials

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 var restify = require('restify'); 
 var builder = require('botbuilder');  
 // Setup Restify Server 
-var server = restify.createServer(); 
-server.listen(process.env.port || process.env.PORT || 3978, 
-function () {    
-    console.log('%s listening to %s', server.name, server.url);  
-});  
-// chat connector for communicating with the Bot Framework Service 
-var connector = new builder.ChatConnector({     
-    appId: "d14a3195-d834-4185-8fad-21248b3d0c8a",     
-    appPassword: "mrnrDN2+%dmsXGDPE2814~*"
-});
-// Listen for messages from users  
-server.post('/api/messages', connector.listen());  
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:') 
-var bot = new builder.UniversalBot(connector, function (session) {     
-session.send("You said: %s", session.message.text); 
-});
 
 
-server.get('/', (req, res, next) => {
-		res.send("HOME OF APP")
-	});
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
+});
+// Create chat bot
+var connector = new builder.ChatConnector
+({ appId: null, appPassword: null }); 
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
+
